96a32b1a28612567531feac747a90953316c432d
mr-ayman <aganiayman063@gmail.com>
2026-03-17 23:07:41 +0530

    updated portfolio

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,27 +1,32 @@
 import "./herosection.css";
-import heroimage from 'src/assets/hero-profile.png';
+import heroprofile from 'src/assets/hero-profile.png';
+import heroimage1 from 'src/assets/heroimg1.svg';
+import heroimage2 from 'src/assets/heroimg2.svg';
+import heroimage3 from 'src/assets/heroimg3.svg';
+import heroimage4 from 'src/assets/heroimg4.svg';
+import heroimage5 from 'src/assets/heroimg5.svg';
 
 
 const HeroSection = () =>{
     return (
         <section className="HeroSection">
             <div className="hero-left">
-                <img className="hero-image" src={heroimage} alt="" />
+                <img className="hero-image" src={heroprofile} alt="" />
             </div>
             <div className="hero-right">
                 <h2 className="hero-name">I’m Abdul Gani Ayman</h2>
                 <h2 className="hero-description1">Full Stack - <span>Developer</span> and UI/UX</h2>
                 <h2 className="hero-description2"><span>Freelancer</span></h2>
                 <div className="hero-links">
-                    <a href="https://dribbble.com/_Ayman_00" target="_blank"><img src="src/assets/heroimg1.svg" alt="" /></a>
+                    <a href="https://dribbble.com/_Ayman_00" target="_blank"><img src={heroimage1} alt="" /></a>
                     <div class="v-line"></div>
-                    <a href="https://www.instagram.com/_ayman_0.0/" target="_blank"><img src="src/assets/heroimg2.svg" alt="" /></a>
+                    <a href="https://www.instagram.com/_ayman_0.0/" target="_blank"><img src={heroimage2} alt="" /></a>
                     <div class="v-line"></div>
-                    <a href="https://x.com/GaniAyman80448" target="_blank"><img src="src/assets/heroimg3.svg" alt="" /></a>
+                    <a href="https://x.com/GaniAyman80448" target="_blank"><img src={heroimage3} alt="" /></a>
                     <div class="v-line"></div>
-                    <a href="https://github.com/mr-ayman" target="_blank"><img src="src/assets/heroimg4.svg" alt="" /></a>
+                    <a href="https://github.com/mr-ayman" target="_blank"><img src={heroimage4} alt="" /></a>
                     <div class="v-line"></div>
-                    <a href="https://www.linkedin.com/in/abdul-gani-ayman-60549b290/" target="_blank"><img src="src/assets/heroimg5.svg" alt="" /></a>
+                    <a href="https://www.linkedin.com/in/abdul-gani-ayman-60549b290/" target="_blank"><img src={heroimage5} alt="" /></a>
                     {/* <div class="v-line"></div>
                     <a href=""><img src="src/assets/heroimg6.svg" alt="" /></a> */}
                 </div>
